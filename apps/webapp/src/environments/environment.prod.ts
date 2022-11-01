52805299609c17720d4e4e1597f40f89a13fc39b
Leton Atannon <leton.atannon@kaibee.fr>
2022-11-01 01:12:19 +0100

    fix callback urls

--- apps/webapp/src/environments/environment.prod.ts
+++ apps/webapp/src/environments/environment.prod.ts
@@ -4,9 +4,9 @@ export const environment = {
   authConfig: {
     domain: 'dev-bata.eu.auth0.com',
     clientId: '53tGhIBsmv1zPnS7Zne8cBY9XqdySKi0',
-    redirectUri: 'https://main.d2s7lfd8fx3bhd.amplifyapp.com/loggedIn',
+    redirectUri: 'https://dev.bellannonces.com/loggedIn',
     audience: 'https://base-api/',
-    logoutUrl: 'https://main.d2s7lfd8fx3bhd.amplifyapp.com'
+    logoutUrl: 'https://dev.bellannonces.com'
   },
   apiBaseUrl: 'https://api-dev.bellannonces.com/api',
   contactApi: {
